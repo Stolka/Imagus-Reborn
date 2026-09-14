4606ec94a8572a03e9aec5d041f548bdef34c5fb
hababr <hhababr@gmail.com>
2026-09-14 17:56:32 +0300

    fix: do not re-init tab on URL change

--- src/background/service.js
+++ src/background/service.js
@@ -825,9 +825,9 @@ chrome.action.onClicked.addListener(toggleTab);
 
 // update badge on tab update
 chrome.tabs.onUpdated.addListener(function (tabId, changeInfo, tab) {
-    if (changeInfo.url) {
+    /* if (changeInfo.url) {
         chrome.tabs.sendMessage(tabId, { cmd: "reinit" }).catch(() => {});
-    }
+    } */
 
     if (!tab.active) return;
     updateBadge(tabId, tab.url);
